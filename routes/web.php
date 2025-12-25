bb5ec6a71a17efe547be1d15871f0f7f4630a8fb
hariz00123-telkom <hariz00123@gmail.com>
2025-12-25 14:28:56 +0700

    Atsir

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index'); // Ini merujuk ke resources/views/index.blade.php
 });
+use App\Http\Controllers\UserController;
+
+// Route untuk memproses update profil
+Route::put('/profile/update', [UserController::class, 'update'])->name('profile.update');
