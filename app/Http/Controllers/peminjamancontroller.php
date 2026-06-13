2ef959080015b945ff23f9c64394a7195129f47e
Zackyaghaatsa04 <susizul0@gmail.com>
2026-06-13 20:06:36 +0700

    Rename PeminjamanController class to peminjamancontroller

--- app/Http/Controllers/peminjamancontroller.php
+++ app/Http/Controllers/peminjamancontroller.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Peminjaman;
 
-class PeminjamanController extends Controller
+class peminjamancontroller extends Controller
 {
     public function store(Request $request)
     {
